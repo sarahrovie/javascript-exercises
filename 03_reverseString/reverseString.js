f17c481a9401cdbbe5da187409ff910d3b847cbd
Sarah <sarahrdvieira@gmail.com>
2025-01-29 20:13:42 -0300

    Pass first test, exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverse = '';
+    let array = string.split('');
 
+    for (let i = array.length; i > 0; i--) {
+        let lastChar = array.pop();
+        reverse += lastChar;
+    }
+    return reverse;
 };
 
 // Do not edit below this line
