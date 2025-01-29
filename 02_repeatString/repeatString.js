46ad1996a5c19ca8d34463ca7b7f392264241a31
Sarah <sarahrdvieira@gmail.com>
2025-01-29 19:39:32 -0300

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, n) {
+    let string = '';
 
+    for (let i = 0; i < n; i++) {
+        string += word;
+    }
+
+    if (n < 0) {
+        return 'ERROR';
+    } else {
+        return string;
+    }
 };
 
 // Do not edit below this line
