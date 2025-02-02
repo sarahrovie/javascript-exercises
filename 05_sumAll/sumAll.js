6d71719e2c03526c731514597a0926ceadf064ff
Sarah <sarahrdvieira@gmail.com>
2025-02-02 20:18:53 -0300

    Finish exercise 05, all test pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    let sum = 0;
+    let start;
+    let end;
 
+    if (n1 < 0 || n2 < 0 || !Number.isInteger(n1) || !Number.isInteger(n2)) {
+        return 'ERROR';
+    }
+
+    if (n1 < n2) {
+        start = n1;
+        end = n2;
+    } else if (n2 < n1) {
+        start = n2;
+        end = n1;
+    }
+
+    sum = (start + end) * (end - start + 1) / 2;
+
+    return sum;
 };
 
 // Do not edit below this line
