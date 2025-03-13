6383f5bd670d6bf55d5316bb14ac1372440f8586
Sarah <sarahrdvieira@gmail.com>
2025-03-13 02:38:40 -0300

    Finish exercise 08, all tests pass

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, num) => total += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total *= num);
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+  return n1 ** n2;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n < 0) {
+    return -1;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return (n * factorial(n - 1));
+  }
 };
 
 // Do not edit below this line
