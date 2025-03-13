61d4d1f22f86b841645884b4816b3084f730f1ab
Sarah <sarahrdvieira@gmail.com>
2025-03-13 03:50:18 -0300

    Finish exercie 9, all tests pass

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz-123456789';
 
+    const cleanedStr = str
+    .toLowerCase()
+    .split('')
+    .filter((char) => alphanumerical.includes(char))
+    .join('');
+
+    const reversedStr = cleanedStr.split('').reverse().join('');
+
+    return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
