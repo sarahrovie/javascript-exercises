dd430d7b9054dc6181c46d5de8d5a88e74376500
Sarah <sarahrdvieira@gmail.com>
2025-03-13 20:29:25 -0300

    Finish exercise 10, all tests pass

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(numArg) {
+    num = Number(numArg);
+    let arr = [0, 1];
 
+    if (num < 0) return 'OOPS';
+    if (num == 0) return 0;
+
+    for (let i = 2; i <= num; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    return arr[num];
 };
 
 // Do not edit below this line
