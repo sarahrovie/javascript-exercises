c0a9bdbe6af9ab584cadc6e0ff2214ccf0d71a37
Sarah <sarahrdvieira@gmail.com>
2025-03-13 22:07:04 -0300

    Finish exercise 12, all tests pass

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+    person.yearOfDeath ??= new Date().getFullYear();
+    
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+        // let oldestAge = !oldest.yearOfDeath ?
+        // year - oldest.yearOfBirth :
+        // oldest.yearOfDeath - oldest.yearOfBirth;
+        // let currentAge = !current.yearOfDeath ?
+        // year - current.yearOfBirth :
+        // current.yearOfDeath - current.yearOfBirth;
+
+        const oldestAge = getAge(oldest);
+        const currentAge = getAge(current);
+        
+        return oldestAge > currentAge ? oldest : current;
+    }, {})
 };
 
 // Do not edit below this line
